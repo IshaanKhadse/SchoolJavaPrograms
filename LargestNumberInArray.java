6082105eecc35940af155bb9bff6d35d4820828f
KanhaKorgaonkar <83264637+KanhaKorgaonkar@users.noreply.github.com>
2021-08-25 04:47:23 -0700

    Create LargestNumberInArray.java

---
+++ LargestNumberInArray.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class questionOne {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner (System.in);
+        int num = 
+        int arr[] = new int [k];
+        for (int i = 0; i < arr.length; i++) {
+           if(arr[i] > max)
+               max = arr[i];
+        }
+        System.out.println("Largest number: " + max);
+    }
+}
