0ea78833d563181ab51f7d9500186031b186b704
KanhaKorgaonkar <83264637+KanhaKorgaonkar@users.noreply.github.com>
2021-08-25 04:49:49 -0700

    Create PrimeFunction.java

---
+++ PrimeFunction.java
@@ -0,0 +1,24 @@
+/* Write a Program using method Boolean isPrime(int x) which checks if x is 
+prime or not. The method returns true if x is prime else returns false.
+Write main() to accept a number, pass it to the method and display the result
+with proper message. */
+
+import java.util.*;
+public class PrimeFunction {
+    boolean isPrime(int x) {
+        int sum=0;
+        for (int i = 0; i<a.length(); i++)
+        {
+            char ch=a.charAt(i);
+            int c=(int)ch;
+            sum = sum + c;
+        }
+        System.out.println("The sum is " + sum);
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the word");
+        String k = sc.next();
+        sum(k);
+    }
+}
