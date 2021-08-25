b9811b073f7f49b6e92b86261cfde998c5d66f93
KanhaKorgaonkar <83264637+KanhaKorgaonkar@users.noreply.github.com>
2021-08-25 04:57:08 -0700

    Create BooleanCheck.java

---
+++ BooleanCheck.java
@@ -0,0 +1,15 @@
+/* Write a program using function boolean check(String k) which returns true if string k is in uppercase or not.
+write main() to accept a word, pass it to the method check() and display the result with the proper */
+
+import java.util.*;
+public class BooleanCheck{
+    public static void main(String[] args){ 
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter word");
+        String word = sc.next();
+        } 
+    public static boolean check(String k){
+        int length = word.length();
+
+    }
+}
