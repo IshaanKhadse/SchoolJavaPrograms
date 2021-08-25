357dd490e959efe3a448477aafe8fafcdb2dff07
KanhaKorgaonkar <83264637+KanhaKorgaonkar@users.noreply.github.com>
2021-08-25 04:59:23 -0700

    Create OverloadingNotes.java

---
+++ OverloadingNotes.java
@@ -0,0 +1,12 @@
+Multiple functions with the same name, but different signature.
+known as polymorphism (multiple forms)
+OOP principle
+Two Types of Polymorphism -
+Overloading, Overriding.
+Overriding happens during inheritance, dynamic binding.
+
+Principle is polymorphism
+
+The methods exist in the same class,
+static binding happens during overloading (matching of method
+call with the appropriate method signature) on compilation.
